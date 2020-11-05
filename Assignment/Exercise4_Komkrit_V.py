931bb080cb5a805f7ff4b53867d2061c58b96d02
Rokoshan <73995727+Rokoshan@users.noreply.github.com>
2020-11-05 18:24:48 +0700

    Add files via upload

---
+++ Assignment/Exercise4_Komkrit_V.py
@@ -0,0 +1,16 @@
+"""
+    FE ย่อมาจาก Foundation English
+    GB ย่อมาจาก General Business
+    ICS ย่อมาจาก Introduction to Computer System
+    CP ย่อมาจาก Computer Programming
+"""
+FE = 100
+GB = 100
+ICS = 100
+CP = 100
+
+print("--- Your Score ---")
+print("Foundation English : ", FE)
+print("General Business : ", GB)
+print("Introduction to Computer System : ", ICS)
+print("Computer Programming : ", CP)
