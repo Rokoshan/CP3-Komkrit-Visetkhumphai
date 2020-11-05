0db0145fc476eac87e6f0e0b35d05335a81d57ef
Rokoshan <73995727+Rokoshan@users.noreply.github.com>
2020-11-05 21:39:52 +0700

    Add files via upload
    Assignment on Exercise 5

---
+++ Assignment/Exercise5_1_Komkrit_V.py
@@ -0,0 +1,7 @@
+firstNum = 10
+secondNum = 2
+
+print(firstNum, "+", secondNum, " =", firstNum+secondNum)
+print(firstNum, "-", secondNum, " =", firstNum-secondNum)
+print(firstNum, "*", secondNum, " =", firstNum*secondNum)
+print(firstNum, "/", secondNum, " =", int(firstNum/secondNum))
